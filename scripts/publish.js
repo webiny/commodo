42dbad79c4263c3e5d6155bf5226abef4c05481d
Adrian Smijulj <adrian1358@gmail.com>
2019-04-24 09:03:54 +0200

    chore: simple publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,9 +5,9 @@ const VERDACCIO_NPM_REGISTRY = "http://localhost:4873";
 
 (async function() {
     console.log(chalk.green("Building all packages..."));
-    await execa("lerna", ["exec", "--", "rm", "-rf", "dist"]);
+    await execa.shell("lerna exec -- rm -rf dist", ["exec", "--", "rm", "-rf", "dist"]);
+    await execa.shell("lerna run build");
 
-    await execa("lerna", ["run", "build"]);
     // TODO: publish to Verdaccio
     // console.log(chalk.green('Publishing packages to Verdaccio...'));
     // await execa("lerna", ["publish", "--conventional-commits", "--yes", ]).stdout.pipe(process.stdout);
@@ -16,20 +16,31 @@ const VERDACCIO_NPM_REGISTRY = "http://localhost:4873";
     // console.log(chalk.green("Testing ..."));
     // await execa("yarn", ["test:jest:dist"]).stdout.pipe(process.stdout);
 
-    // console.log(response)
-    //console.log(chalk.green('Starting Verdaccio...'));
+    console.log(chalk.green("Creating a new version..."));
+    const { stdout, stderr } = await execa.shell("lerna version --conventional-commits --yes");
+
+    if (stderr) {
+        console.log(chalk.red(stderr));
+        return;
+    }
+
+    console.log(stdout);
 
     console.log(chalk.green("Copying non-code files..."));
-    await execa("lerna", ["exec", "--", "cp", "package.json", "dist/package.json"]).stdout.pipe(
-        process.stdout
+    const files = ["package.json", "README.md"];
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        await execa.shell(`lerna exec -- cp ${file} dist/${file}`);
+    }
+
+    console.log(chalk.green("Publishing to NPM..."));
+    const publishResults = await execa.shell(
+        "lerna publish --conventional-commits --yes --contents dist"
     );
 
-    console.log(chalk.green("Publishing packages..."));
-    await execa("lerna", [
-        "publish",
-        "--conventional-commits",
-        "--yes",
-        "--contents",
-        "dist"
-    ]).stdout.pipe(process.stdout);
+    if (publishResults.stderr) {
+        console.log(chalk.red(publishResults.stderr));
+    } else {
+        console.log(publishResults.stdout);
+    }
 })();
