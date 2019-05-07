4d7857fa6dc1f368544b2f03ccc4aa25a6ed022d
Adrian Smijulj <adrian1358@gmail.com>
2019-05-07 17:33:31 +0200

    test: improve tests

--- packages/hooks/__tests__/hooksThisReference.test.js
+++ packages/hooks/__tests__/hooksThisReference.test.js
@@ -4,12 +4,22 @@ import { withProps } from "repropose";
 
 test(`"this" used in hooks must be a correct reference`, async () => {
     const TestModel1 = compose(
+        withProps({
+            number2: 100,
+            async doSomething() {
+                await this.hook("hook1");
+                await this.hook("hook2");
+                return this;
+            }
+        }),
         withHooks({
             hook1() {
-                this.number = this.number + 0.33;
+                this.number = this.number + 33;
+                this.number2 = this.number2 + 20;
             },
             hook2() {
-                this.number = this.number + 0.33;
+                this.number = this.number + 33;
+                this.number2 = this.number2 + 20;
             }
         }),
         withProps({
@@ -22,19 +32,19 @@ test(`"this" used in hooks must be a correct reference`, async () => {
         }),
         withHooks({
             hook1() {
-                console.log("B1");
-                this.number = this.number + 1;
+                this.number = this.number + 10;
+                this.number2 = this.number2 + 25;
             },
             hook2() {
-                console.log("B2");
-                this.number = this.number + 1;
+                this.number = this.number + 10;
+                this.number2 = this.number2 + 25;
             }
         })
     )(function() {});
 
     const testModel1 = new TestModel1();
-    console.log(testModel1.__hooks);
     await testModel1.doSomething();
 
-    expect(testModel1.number).toBe(2.66);
+    expect(testModel1.number).toBe(86);
+    expect(testModel1.number2).toBe(190);
 });
