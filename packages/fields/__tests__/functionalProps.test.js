0b996778cfc37e484496cb31749c7e7ec765e6a5
Adrian Smijulj <adrian1358@gmail.com>
2019-05-07 16:22:08 +0200

    docs: improve tests

---
+++ packages/fields/__tests__/functionalProps.test.js
@@ -0,0 +1,26 @@
+import { withFields, number } from "@commodo/fields";
+import { compose } from "ramda";
+
+test(`should be possible to access current instance as first "withFields" argument`, async () => {
+    const Model = compose(
+        withFields(instance => {
+            const newProps = {};
+            if (!instance.getField("value2")) {
+                newProps["value200"] = number();
+            }
+
+            if (!instance.getField("value3")) {
+                newProps["value300"] = number();
+            }
+
+            return newProps;
+        }),
+        withFields({
+            value1: number(),
+            value2: number()
+        })
+    )(function() {});
+
+    const model = new Model();
+    expect(Object.keys(model.getFields())).toEqual(["value1", "value2", "value300"]);
+});
