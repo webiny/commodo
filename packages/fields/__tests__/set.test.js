1cf27c8dc5c0d0b35a462aa31dc5a0e8fa4f3760
Adrian Smijulj <adrian1358@gmail.com>
2019-05-09 10:37:00 +0200

    fix: internal setting of "set" flag works correctly

--- packages/fields/__tests__/set.test.js
+++ packages/fields/__tests__/set.test.js
@@ -4,6 +4,11 @@ describe("set test", () => {
     test("should set values correctly", async () => {
         const user = new User();
 
+        expect(user.getField("firstName").isSet()).toBe(false);
+        expect(user.getField("lastName").isSet()).toBe(false);
+        expect(user.getField("age").isSet()).toBe(false);
+        expect(user.getField("enabled").isSet()).toBe(false);
+        
         user.firstName = "John";
         user.lastName = "Doe";
         user.age = 12;
@@ -13,5 +18,10 @@ describe("set test", () => {
         expect(user.lastName).toBe("Doe");
         expect(user.age).toBe(12);
         expect(user.enabled).toBe(true);
+
+        expect(user.getField("firstName").isSet()).toBe(true);
+        expect(user.getField("lastName").isSet()).toBe(true);
+        expect(user.getField("age").isSet()).toBe(true);
+        expect(user.getField("enabled").isSet()).toBe(true);
     });
 });
