2ecb94bc04222615803c6d8ab71d0a659b8065fe
Pavel Denisjuk <pavel@webiny.com>
2020-04-13 18:03:18 +0200

    test: configure jest projects

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  "presets": [
-    [
-      "@babel/preset-env",
-      {
-        "debug": false,
-        "targets": {
-          "browsers": [
-            "last 3 versions"
-          ]
-        }
-      }
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                debug: false,
+                targets: {
+                    browsers: ["last 3 versions"]
+                }
+            }
+        ],
+        ["@babel/preset-flow"]
     ],
-      ["@babel/preset-flow"]
-  ],
-    "plugins" : [
-        "@babel/plugin-transform-runtime"
-    ]
+    plugins: ["@babel/plugin-transform-runtime"]
 };
