bf68dfdc4413ed724f7bb4ceb53e69d64b8a83e9
Pavel Denisjuk <pavel@webiny.com>
2020-04-18 18:47:10 +0200

    test: replace mocks with real database calls

---
+++ packages/fields-storage-mongodb/__tests__/delete.test.js
@@ -0,0 +1,30 @@
+import { WithStorageError } from "@commodo/fields-storage";
+import useModels from "./models/useModels";
+import createSimpleModelsMock from "./mocks/createSimpleModelsMock";
+
+describe("delete test", function() {
+    const { models, getCollection } = useModels();
+    const { simpleModelsMock, ids } = createSimpleModelsMock();
+
+    beforeAll(() => getCollection("SimpleModel").insertMany(simpleModelsMock));
+
+    it("should delete a record", async () => {
+        const id = String(ids[0]);
+        let record = await models.SimpleModel.findById(id);
+        await record.delete();
+
+        record = await models.SimpleModel.findById(id);
+        expect(record).toBeNull();
+    });
+
+    it("should throw an exception because model was not previously saved", async () => {
+        try {
+            const simpleModel = new models.SimpleModel();
+            await simpleModel.delete();
+        } catch (e) {
+            expect(e.code).toBe(WithStorageError.CANNOT_DELETE_NO_ID);
+            return;
+        }
+        throw Error(`Error should've been thrown.`);
+    });
+});
---
+++ packages/fields-storage-mongodb/__tests__/find.test.js
@@ -0,0 +1,68 @@
+import useModels from "./models/useModels";
+import createSimpleModelsMock from "./mocks/createSimpleModelsMock";
+
+describe("find test", function() {
+    const { models, getCollection } = useModels();
+    const { simpleModelsMock, ids } = createSimpleModelsMock();
+
+    beforeAll(() => getCollection("SimpleModel").insertMany(simpleModelsMock));
+
+    it("should find all models", async () => {
+        const records = await models.SimpleModel.find();
+
+        expect(records.length).toBe(4);
+    });
+
+    it("should find models using limit", async () => {
+        const records = await models.SimpleModel.find({ limit: 2 });
+
+        expect(records.length).toBe(2);
+    });
+
+    it("should find models using sort, limit and offset", async () => {
+        const stringIds = ids.map(x => String(x));
+        const records = await models.SimpleModel.find({
+            sort: { age: 1 },
+            limit: 2,
+            offset: 2
+        });
+
+        expect(records.length).toBe(2);
+        expect(records[0].id).toBe(stringIds[2]);
+        expect(records[1].id).toBe(stringIds[3]);
+    });
+
+    it("should find models using query and sort", async () => {
+        const stringIds = ids.map(x => String(x));
+        const records = await models.SimpleModel.find({
+            sort: { slug: -1 },
+            query: {
+                enabled: true
+            }
+        });
+
+        expect(records.length).toBe(3);
+        expect(records[0].id).toBe(stringIds[1]);
+        expect(records[1].id).toBe(stringIds[3]);
+        expect(records[2].id).toBe(stringIds[0]);
+    });
+
+    it("should find models using query, sort and search", async () => {
+        const stringIds = ids.map(x => String(x));
+        const records = await models.SimpleModel.find({
+            sort: { slug: 1 },
+            query: {
+                age: { $gt: 10 }
+            },
+            search: {
+                query: "serverless",
+                fields: ["name", "slug"],
+                operator: "or"
+            }
+        });
+
+        expect(records.length).toBe(2);
+        expect(records[0].id).toBe(stringIds[1]);
+        expect(records[1].id).toBe(stringIds[2]);
+    });
+});
