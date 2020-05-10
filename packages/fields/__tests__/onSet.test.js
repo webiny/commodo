cdfa9110c57e1d271680b07c0103dec45118bc8e
Adrian Smijulj <adrian1358@gmail.com>
2020-05-10 11:13:33 +0200

    feat: ensure async callbacks cannot be passed to the onSet function

--- packages/fields/__tests__/onSet.test.js
+++ packages/fields/__tests__/onSet.test.js
@@ -1,26 +1,45 @@
 import { withFields, onSet, number, boolean } from "@commodo/fields";
 import { compose } from "ramda";
 
-test(`should be possible to access other fields in onSet callback`, async () => {
-    const Model = compose(
-        withFields(instance => ({
-            value: onSet(function(value) {
-                return instance.x2Value ? value * 2 : value;
-            })(number()),
-            x2Value: boolean()
-        }))
-    )();
+describe("onSet function test", () => {
+    test(`should be possible to access other fields in onSet callback`, async () => {
+        const Model = compose(
+            withFields(instance => ({
+                value: onSet(function(value) {
+                    return instance.x2Value ? value * 2 : value;
+                })(number()),
+                x2Value: boolean()
+            }))
+        )();
 
-    const model = new Model();
-    model.value = 100;
-    expect(model.value).toBe(100);
+        const model = new Model();
+        model.value = 100;
+        expect(model.value).toBe(100);
 
-    model.value = 200;
-    expect(model.value).toBe(200);
+        model.value = 200;
+        expect(model.value).toBe(200);
 
-    model.x2Value = true;
-    expect(model.value).toBe(200);
+        model.x2Value = true;
+        expect(model.value).toBe(200);
 
-    model.value = 200;
-    expect(model.value).toBe(400);
+        model.value = 200;
+        expect(model.value).toBe(400);
+    });
+
+    test(`should not be able to provide async callbacks`, async () => {
+        const Model = compose(
+            withFields({
+                value: onSet(async () => {})(number())
+            })
+        )();
+
+        const model = new Model();
+        let error;
+        try {
+            model.value = 100;
+        } catch (e) {
+            error = e;
+        }
+        expect(error.message).toBe(`A promise was returned from the "onSet" callback (applied on the "value" field). Provided callbacks cannot perform async operations.`);
+    });
 });
--- packages/fields/src/onSet.js
+++ packages/fields/src/onSet.js
@@ -1,12 +1,22 @@
 // @flow
 import { withProps } from "repropose";
 
+function isPromise(value) {
+    return Boolean(value && typeof value.then === "function");
+}
+
 const onSet = (callback: (value: any) => any) => {
     return withProps(props => {
         const { setValue } = props;
         return {
             setValue(value) {
-                return setValue.call(this, callback(value));
+                const newValue = callback(value);
+                if (isPromise(newValue)) {
+                    throw new Error(
+                        `A promise was returned from the "onSet" callback (applied on the "${props.name}" field). Provided callbacks cannot perform async operations.`
+                    );
+                }
+                return setValue.call(this, newValue);
             }
         };
     });
