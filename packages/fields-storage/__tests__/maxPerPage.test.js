4689f6dcb3b6ece82384e934d87730e3322aab2c
Adrian Smijulj <adrian1358@gmail.com>
2020-09-07 12:10:34 +0200

    chore: rename `maxPerPage` to `maxLimit`

--- packages/fields-storage/__tests__/maxPerPage.test.js
+++ packages/fields-storage/__tests__/maxPerPage.test.js
@@ -4,69 +4,69 @@ import { withName } from "@commodo/name";
 import { compose } from "ramda";
 import { CustomDriver } from "./resources/CustomDriver";
 
-const EntityWithoutMaxPerPage = compose(
+const EntityWithoutMaxLimit = compose(
     withFields({
         name: string()
     }),
-    withName("EntityWithoutMaxPerPage"),
+    withName("EntityWithoutMaxLimit"),
     withStorage({
         driver: new CustomDriver()
     })
 )();
 
-const EntityWithMaxPerPage = compose(
+const EntityWithMaxLimit = compose(
     withFields({
         name: string()
     }),
-    withName("EntityWithMaxPerPage"),
+    withName("EntityWithMaxLimit"),
     withStorage({
         driver: new CustomDriver(),
-        maxPerPage: 500
+        maxLimit: 500
     })
 )();
 
-const EntityWithMaxPerPageSetToNull = compose(
+const EntityWithMaxLimitSetToNull = compose(
     withFields({
         name: string()
     }),
-    withName("EntityWithMaxPerPage"),
+    withName("EntityWithMaxLimit"),
     withStorage({
         driver: new CustomDriver(),
-        maxPerPage: null
+        maxLimit: null
     })
 )();
 
-describe("maxPerPage test", () => {
-    test("must throw errors if maxPerPage config parameter was exceeded", async () => {
-        await EntityWithoutMaxPerPage.find({ limit: 99 });
-        await EntityWithoutMaxPerPage.find({ limit: 100 });
+describe("maxLimit test", () => {
+    test("must throw errors if maxLimit config parameter was exceeded", async () => {
+        await EntityWithoutMaxLimit.find({ limit: 99 });
+        await EntityWithoutMaxLimit.find({ limit: 100 });
 
         let error = null;
         try {
-            await EntityWithoutMaxPerPage.find({ limit: 101 });
+            await EntityWithoutMaxLimit.find({ limit: 101 });
         } catch (e) {
             error = e;
         }
 
         expect(error.message).toBe("Cannot query for more than 100 models per page.");
 
-        await EntityWithMaxPerPage.find({ limit: 499 });
-        await EntityWithMaxPerPage.find({ limit: 500 });
+        await EntityWithMaxLimit.find({ limit: 499 });
+        await EntityWithMaxLimit.find({ limit: 500 });
 
         error = null;
         try {
-            await EntityWithMaxPerPage.find({ limit: 501 });
+            await EntityWithMaxLimit.find({ limit: 501 });
         } catch (e) {
             error = e;
         }
 
         expect(error.message).toBe("Cannot query for more than 500 models per page.");
 
-        await EntityWithMaxPerPageSetToNull.find({ limit: 100 });
+        await EntityWithMaxLimitSetToNull.find({ limit: 100 });
 
         error = null;
         try {
-            await EntityWithoutMaxPerPage.find({ limit: 101 });
+            await EntityWithoutMaxLimit.find({ limit: 101 });
         } catch (e) {
             error = e;
         }
