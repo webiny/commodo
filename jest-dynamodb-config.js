4aba1ba9917d70c395bdd4cd5c437eadd3267cdc
Adrian Smijulj <adrian1358@gmail.com>
2020-09-07 11:57:32 +0200

    chore: create GSIs

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -8,9 +8,43 @@ module.exports = {
             ],
             AttributeDefinitions: [
                 { AttributeName: "pk", AttributeType: "S" },
-                { AttributeName: "sk", AttributeType: "S" }
+                { AttributeName: "sk", AttributeType: "S" },
+                { AttributeName: "gsi1pk", AttributeType: "S" },
+                { AttributeName: "gsi1sk", AttributeType: "S" },
+                { AttributeName: "gsi2pk", AttributeType: "S" },
+                { AttributeName: "gsi2sk", AttributeType: "S" }
             ],
-            ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+            ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+            GlobalSecondaryIndexes: [
+                {
+                    IndexName: "gsi1pk_gsi1sk",
+                    KeySchema: [
+                        { AttributeName: "gsi1pk", KeyType: "HASH" },
+                        { AttributeName: "gsi1sk", KeyType: "RANGE" }
+                    ],
+                    Projection: {
+                        ProjectionType: "ALL"
+                    },
+                    ProvisionedThroughput: {
+                        ReadCapacityUnits: 1,
+                        WriteCapacityUnits: 1
+                    }
+                },
+                {
+                    IndexName: "gsi2pk_gsi2sk",
+                    KeySchema: [
+                        { AttributeName: "gsi2pk", KeyType: "HASH" },
+                        { AttributeName: "gsi2sk", KeyType: "RANGE" }
+                    ],
+                    Projection: {
+                        ProjectionType: "ALL"
+                    },
+                    ProvisionedThroughput: {
+                        ReadCapacityUnits: 1,
+                        WriteCapacityUnits: 1
+                    }
+                }
+            ]
         }
     ]
 };
