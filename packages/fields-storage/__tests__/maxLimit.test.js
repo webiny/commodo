1d1f650917381ad4ac3a2ca8b6492bc28eec9422
Adrian Smijulj <adrian1358@gmail.com>
2020-09-11 13:17:14 +0200

    test: adjust to recent maxLimit changes

--- packages/fields-storage/__tests__/maxLimit.test.js
+++ packages/fields-storage/__tests__/maxLimit.test.js
@@ -25,52 +25,18 @@ const EntityWithMaxLimit = compose(
     })
 )();
 
-const EntityWithMaxLimitSetToNull = compose(
-    withFields({
-        name: string()
-    }),
-    withName("EntityWithMaxLimit"),
-    withStorage({
-        driver: new NeDbDriver(),
-        maxLimit: null
-    })
-)();
-
 describe("maxLimit test", () => {
     test("must throw errors if maxLimit config parameter was exceeded", async () => {
         await EntityWithoutMaxLimit.find({ limit: 99 });
-        await EntityWithoutMaxLimit.find({ limit: 100 });
+        await EntityWithoutMaxLimit.find({ limit: 999999 });
 
         let error = null;
-        try {
-            await EntityWithoutMaxLimit.find({ limit: 101 });
-        } catch (e) {
-            error = e;
-        }
-
-        expect(error.message).toBe(`Cannot set a limit greater than 100. Please adjust the "maxLimit" argument if needed.`);
-
-        await EntityWithMaxLimit.find({ limit: 499 });
-        await EntityWithMaxLimit.find({ limit: 500 });
-
-        error = null;
         try {
             await EntityWithMaxLimit.find({ limit: 501 });
         } catch (e) {
             error = e;
         }
 
-        expect(error.message).toBe(`Cannot set a limit greater than 500. Please adjust the "maxLimit" argument if needed.`);
-
-        await EntityWithMaxLimitSetToNull.find({ limit: 100 });
-
-        error = null;
-        try {
-            await EntityWithoutMaxLimit.find({ limit: 101 });
-        } catch (e) {
-            error = e;
-        }
-
-        expect(error.message).toBe(`Cannot set a limit greater than 100. Please adjust the "maxLimit" argument if needed.`);
+        expect(error.message).toBe(`Cannot set a limit greater than "500". Please adjust the "maxLimit" argument if needed.`);
     });
 });
