c78816fd09e5ee0c71becc5a640377c8199cce07
Adrian Smijulj <adrian1358@gmail.com>
2020-09-13 15:42:58 +0200

    fix: remove `Collection` class

--- packages/fields-storage/src/index.js
+++ packages/fields-storage/src/index.js
@@ -1,6 +1,5 @@
 export { default as withStorage } from "./withStorage";
 export { default as WithStorageError } from "./WithStorageError";
-export { default as Collection } from "./Collection";
 export { default as StoragePool } from "./StoragePool";
 export { default as StoragePoolEntry } from "./StoragePoolEntry";
 export { default as hasWithStorage } from "./hasWithStorage";
