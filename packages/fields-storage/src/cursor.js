3847910bbf64dac3eebd30ee060c735f177dd856
Adrian Smijulj <adrian1358@gmail.com>
2020-10-06 15:14:24 +0200

    fix: if cursor property is an ISODate, convert it into a Date object

--- packages/fields-storage/src/cursor.js
+++ packages/fields-storage/src/cursor.js
@@ -11,5 +11,23 @@ export const decodeCursor = cursor => {
         return null;
     }
 
-    return JSON.parse(Buffer.from(cursor, "base64").toString("ascii"));
+    const value = JSON.parse(Buffer.from(cursor, "base64").toString("ascii"));
+
+    if (value && typeof value === "object") {
+        for (let key in value) {
+            if (typeof key !== "string") {
+                continue;
+            }
+
+            const possibleDate = new Date(value[key]);
+            try {
+                if (possibleDate.toISOString() === value[key]) {
+                    value[key] = possibleDate;
+                }
+            } catch {
+                // Do nothing.
+            }
+        }
+    }
+    return value;
 };
